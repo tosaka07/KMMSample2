d3e5af5ca161e59cce5ad179b317fed408639230
tosaka07 <tosaka.tech@gmail.com>
2021-04-06 18:02:43 +0900

    Update redundant companion object

--- shared/src/commonMain/kotlin/cam/inc/kmm2/shared/model/request/PostApi.kt
+++ shared/src/commonMain/kotlin/cam/inc/kmm2/shared/model/request/PostApi.kt
@@ -4,12 +4,10 @@ import cam.inc.kmm2.shared.model.api.HTTPMethod
 import cam.inc.kmm2.shared.model.api.Request
 import cam.inc.kmm2.shared.model.entity.Post
 
-class PostApi {
-    companion object {
-        fun posts(): Request<List<Post>> {
-            val path = "/posts"
-            val method = HTTPMethod.Get
-            return Request(path, method)
-        }
+object PostApi {
+    fun posts(): Request<List<Post>> {
+        val path = "/posts"
+        val method = HTTPMethod.Get
+        return Request(path, method)
     }
 }
